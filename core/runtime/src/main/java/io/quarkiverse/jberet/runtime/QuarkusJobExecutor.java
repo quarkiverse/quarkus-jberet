e81613cf9ed75ae44b21d97ea0db60c4e6487b7e
jint128 <kalyan.dass.kairamkonda@jbhunt.com>
2022-01-17 17:17:09 -0600

    Resolved PR comments to have constant cpuPoolSize instead of calculating at every go.

--- core/runtime/src/main/java/io/quarkiverse/jberet/runtime/QuarkusJobExecutor.java
+++ core/runtime/src/main/java/io/quarkiverse/jberet/runtime/QuarkusJobExecutor.java
@@ -4,18 +4,20 @@ import io.quarkus.runtime.ThreadPoolConfig;
 import org.jberet.spi.JobExecutor;
 import org.wildfly.common.cpu.ProcessorInfo;
 
+import java.util.OptionalInt;
 import java.util.concurrent.Executor;
 
 class QuarkusJobExecutor extends JobExecutor {
-    private final int cpuPoolSize;
+    private final OptionalInt maxThreads;
+    private final int cpuPoolSize = Math.max(8 * ProcessorInfo.availableProcessors(), 200);
 
     public QuarkusJobExecutor(Executor delegate, final ThreadPoolConfig threadPoolConfig) {
         super(delegate);
-        this.cpuPoolSize = threadPoolConfig.maxThreads.orElse(Math.max(8 * ProcessorInfo.availableProcessors(), 200));
+        this.maxThreads = threadPoolConfig.maxThreads;
     }
 
     @Override
     protected int getMaximumPoolSize() {
-        return cpuPoolSize;
+        return maxThreads.orElse(cpuPoolSize);
     }
 }
