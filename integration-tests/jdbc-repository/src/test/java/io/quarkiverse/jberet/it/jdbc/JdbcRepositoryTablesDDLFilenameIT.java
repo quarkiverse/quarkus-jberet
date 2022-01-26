7c0336535149eb951961c3b6373a7ee07a5302ea
Luca Basso Ricci <Luca.BassoRicci@zucchetti.it>
2022-01-26 17:08:31 +0100

    NI tests
    Removed native-image tests dynamic profile based because tested
    properties cannot be changed during runtime

--- integration-tests/jdbc-repository/src/test/java/io/quarkiverse/jberet/it/jdbc/JdbcRepositoryTablesDDLFilenameIT.java
+++
@@ -1,9 +0,0 @@
-package io.quarkiverse.jberet.it.jdbc;
-
-import io.quarkus.test.junit.NativeImageTest;
-import io.quarkus.test.junit.TestProfile;
-
-@NativeImageTest
-@TestProfile(JdbcRepositoryTablesDDLFilenameTest.Profile.class)
-public class JdbcRepositoryTablesDDLFilenameIT extends JdbcRepositoryTablesDDLFilenameTest {
-}
--- integration-tests/jdbc-repository/src/test/java/io/quarkiverse/jberet/it/jdbc/JdbcRepositoryTablesPrefixSuffixIT.java
+++
@@ -1,9 +0,0 @@
-package io.quarkiverse.jberet.it.jdbc;
-
-import io.quarkus.test.junit.NativeImageTest;
-import io.quarkus.test.junit.TestProfile;
-
-@NativeImageTest
-@TestProfile(JdbcRepositoryTablesPrefixSuffixTest.Profile.class)
-public class JdbcRepositoryTablesPrefixSuffixIT extends JdbcRepositoryTablesPrefixSuffixTest {
-}
