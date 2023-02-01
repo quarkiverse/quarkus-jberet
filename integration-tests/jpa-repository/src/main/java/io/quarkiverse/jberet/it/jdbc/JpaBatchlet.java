5384dfc038ddae070520e44dad5ab49fa6def41d
Alessandro Moscatelli <alessandro.moscatelli@live.com>
2023-02-01 13:19:50 +0100

    jpa job repository integration

---
+++ integration-tests/jpa-repository/src/main/java/io/quarkiverse/jberet/it/jdbc/JpaBatchlet.java
@@ -0,0 +1,24 @@
+package io.quarkiverse.jberet.it.jdbc;
+
+import jakarta.batch.api.AbstractBatchlet;
+import jakarta.batch.runtime.BatchStatus;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Named;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Dependent
+@Named
+public class JpaBatchlet extends AbstractBatchlet {
+    @ConfigProperty(name = "jpa.batchlet.fail", defaultValue = "false")
+    Boolean fail;
+
+    @Override
+    public String process() {
+        if (fail) {
+            throw new RuntimeException();
+        }
+
+        return BatchStatus.COMPLETED.toString();
+    }
+}
---
+++ integration-tests/jpa-repository/src/test/java/io/quarkiverse/jberet/it/jdbc/JpaRepositoryIT.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.jberet.it.jdbc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class JpaRepositoryIT extends JpaRepositoryTest {
+}
