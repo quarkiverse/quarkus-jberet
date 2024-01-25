1b3fbc31abf9662eea7efed0a973e86939725d89
Roberto Cortez <radcortez@yahoo.com>
2024-01-25 00:30:43 +0000

    Use Quarkus InsecureRequests strategy (#273)

--- rest/runtime/src/main/java/io/quarkiverse/jberet/rest/runtime/JBeretRestProducer.java
+++ rest/runtime/src/main/java/io/quarkiverse/jberet/rest/runtime/JBeretRestProducer.java
@@ -1,26 +1,32 @@
 package io.quarkiverse.jberet.rest.runtime;
 
+import static io.quarkus.vertx.http.runtime.HttpConfiguration.InsecureRequests.ENABLED;
+
 import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jberet.rest.client.BatchClient;
 
 import io.quarkus.arc.DefaultBean;
+import io.quarkus.vertx.http.runtime.HttpBuildTimeConfig;
+import io.quarkus.vertx.http.runtime.HttpConfiguration;
+import io.quarkus.vertx.http.runtime.HttpConfiguration.InsecureRequests;
+import io.quarkus.vertx.http.runtime.options.HttpServerOptionsUtils;
 
 public class JBeretRestProducer {
-    @ConfigProperty(name = "quarkus.http.host")
-    String host;
-    @ConfigProperty(name = "quarkus.http.port")
-    int port;
-    @ConfigProperty(name = "quarkus.http.insecure-requests")
-    String insecure;
+    @Inject
+    HttpBuildTimeConfig httpBuildTimeConfig;
+    @Inject
+    HttpConfiguration httpConfiguration;
 
     @Produces
     @DefaultBean
     @Singleton
     public BatchClient batchClient() {
-        final String scheme = "enabled".equals(insecure) ? "http" : "https";
-        return new BatchClient(scheme + "://" + host + ":" + port);
+        InsecureRequests insecureRequestStrategy = HttpServerOptionsUtils.getInsecureRequestStrategy(httpBuildTimeConfig,
+                httpConfiguration.insecureRequests);
+        String scheme = insecureRequestStrategy.equals(ENABLED) ? "http" : "https";
+        return new BatchClient(scheme + "://" + httpConfiguration.host + ":" + httpConfiguration.port);
     }
 }
