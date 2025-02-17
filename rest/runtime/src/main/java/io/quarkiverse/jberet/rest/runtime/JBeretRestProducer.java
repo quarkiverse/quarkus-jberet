70fa46dbff7b8064e97a0ca4b6864c19cd774fa0
Roberto Cortez <radcortez@yahoo.com>
2025-02-17 17:10:08 +0000

    Update to Quarkus 3.19.0

--- rest/runtime/src/main/java/io/quarkiverse/jberet/rest/runtime/JBeretRestProducer.java
+++ rest/runtime/src/main/java/io/quarkiverse/jberet/rest/runtime/JBeretRestProducer.java
@@ -1,6 +1,6 @@
 package io.quarkiverse.jberet.rest.runtime;
 
-import static io.quarkus.vertx.http.runtime.HttpConfiguration.InsecureRequests.ENABLED;
+import static io.quarkus.vertx.http.runtime.VertxHttpConfig.InsecureRequests.ENABLED;
 
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
@@ -9,24 +9,23 @@ import jakarta.inject.Singleton;
 import org.jberet.rest.client.BatchClient;
 
 import io.quarkus.arc.DefaultBean;
-import io.quarkus.vertx.http.runtime.HttpBuildTimeConfig;
-import io.quarkus.vertx.http.runtime.HttpConfiguration;
-import io.quarkus.vertx.http.runtime.HttpConfiguration.InsecureRequests;
+import io.quarkus.vertx.http.runtime.VertxHttpBuildTimeConfig;
+import io.quarkus.vertx.http.runtime.VertxHttpConfig;
 import io.quarkus.vertx.http.runtime.options.HttpServerOptionsUtils;
 
 public class JBeretRestProducer {
     @Inject
-    HttpBuildTimeConfig httpBuildTimeConfig;
+    VertxHttpBuildTimeConfig httpBuildTimeConfig;
     @Inject
-    HttpConfiguration httpConfiguration;
+    VertxHttpConfig httpConfig;
 
     @Produces
     @DefaultBean
     @Singleton
     public BatchClient batchClient() {
-        InsecureRequests insecureRequestStrategy = HttpServerOptionsUtils.getInsecureRequestStrategy(httpBuildTimeConfig,
-                httpConfiguration.insecureRequests);
+        VertxHttpConfig.InsecureRequests insecureRequestStrategy = HttpServerOptionsUtils
+                .getInsecureRequestStrategy(httpBuildTimeConfig, httpConfig.insecureRequests());
         String scheme = insecureRequestStrategy.equals(ENABLED) ? "http" : "https";
-        return new BatchClient(scheme + "://" + httpConfiguration.host + ":" + httpConfiguration.port);
+        return new BatchClient(scheme + "://" + httpConfig.host() + ":" + httpConfig.port());
     }
 }
